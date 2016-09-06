abf956e02fc5b18e08371f42f69acfb01d1ed43b
Vladimir Tyrin <b0-0b@yandex.ru>
2016-09-06 19:59:51 +0300

    Base class for bots added

---
+++ TicTacToeGame.Players/BotPlayer.cs
@@ -0,0 +1,13 @@
+﻿using TicTacToeGame.Common.Enums;
+using TicTacToeGame.Common.Interfaces;
+using TicTacToeGame.Common.Utils;
+
+namespace TicTacToeGame.Players
+{
+    public abstract class BotPlayer : IPlayer
+    {
+        public abstract string Name { get; } 
+        public PlayerType Type => PlayerType.Bot;
+        public abstract Cell GetNextMove(FieldState fieldState);
+    }
+}

e8524df117dd00d1dae1cabc8c57b27aa0318e19
Vladimir Tyrin <b0-0b@yandex.ru>
2016-09-06 20:14:03 +0300

    Small enhancement

--- TicTacToeGame.Players/BotPlayer.cs
+++ TicTacToeGame.Players/BotPlayer.cs
@@ -6,9 +6,13 @@ namespace TicTacToeGame.Players
 {
     public abstract class BotPlayer : IPlayer
     {
+        #region IPlayer
+
         public abstract string Name { get; } 
         public PlayerType Type => PlayerType.Bot;
         public abstract Cell GetNextMove(FieldState fieldState);
+
+        #endregion
 
         // These will be shown in bot competitions
         public virtual string Description => null;

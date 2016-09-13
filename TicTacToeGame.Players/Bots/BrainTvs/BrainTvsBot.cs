4570d1a04d7c565d4682c3cf6b49d2958d6b0592
Vladimir.Tyrin <b0-0b@yandex.ru>
2016-09-13 16:19:03 +0300

    New bot added

---
+++ TicTacToeGame.Players/Bots/BrainTvs/BrainTvsBot.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using TicTacToeGame.Common.Enums;
+using TicTacToeGame.Common.Utils;
+
+namespace TicTacToeGame.Players.Bots.BrainTvs
+{
+    internal class BrainTvsBot : BotPlayer
+    {
+        #region BotPlayer
+        public override string Name => "bra1n_tvs bot";
+        public override Cell GetNextMove(FieldState fieldState)
+        {
+            while (true)
+            {
+                var x = _random.Next(fieldState.Height);
+                var y = _random.Next(fieldState.Width);
+                if (fieldState.Field[x, y] == CellSign.Empty)
+                    return new Cell(x, y);
+            }
+        }
+
+        public override string Author => "Vladimir Tyrin";
+        #endregion
+
+        #region private
+        private readonly Random _random = new Random();
+        #endregion
+    }
+}
